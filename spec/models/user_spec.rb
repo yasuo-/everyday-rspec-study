a68f6200683a7b46ffad86b4ce3958855e939c4b
yasuo- <breitling1216@gmail.com>
2018-08-22 17:05:03 +0900

    Add: project spec test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -27,7 +27,23 @@ RSpec.describe User, type: :model do
     user.valid?
     expect(user.errors[:last_name]).to include("can't be blank")
   end
-  it "is invalid without an email address"
+  # 重複したメールアドレスなら無効な状態であること
+  it "is invalid without an email address" do
+    User.create(
+      first_name: "Ken",
+      last_name:  "Tester",
+      email:      "tester@example.com",
+      password:   "dottle-nouveau-pavilion",
+    )
+    user = User.new(
+      first_name: "Ken",
+      last_name:  "Tester",
+      email:      "tester@example.com",
+      password:   "dottle-nouveau-pavilion",
+    )
+    user.valid?
+    expect(user.errors[:email]).to include("has already been taken")
+  end
   it "is invalid with a duplicate email address"
   it "returns a user's full name as a string"
 end
